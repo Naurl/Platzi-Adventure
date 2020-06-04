4dc4086d898797dedb7a09893b2e110bb824b642
Nahuel Venencio <nahuelvenencio@gmail.com>
2020-06-04 02:01:55 -0300

    Manejo de animaciones con el componente animator, parametros y blend tree para un manejo fluido en la interseccion de animaciones. Nota: Hay un pequeño bug por pocos frames cuando intentamos que el personaje quede mirando hacia el ultimo lado al que se movio en el cual se intercalan otras animaciones por un corto espacio de tiempo.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,29 +6,48 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 4.0f;
+    private bool isWalking = false;
+    public Vector2 lastMovement = Vector2.zero;
 
     private const string horizontal = "Horizontal";
     private const string vertical = "Vertical";
+    private const string animp_isWalking = "IsWalking";
+    private const string animp_LastHorizontal = "LastHorizontal";
+    private const string animp_LastVertical = "LastVertical";
+
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // speed = velocity*time;
+        isWalking = false;
 
-        if(Math.Abs(Input.GetAxisRaw(horizontal)) > 0.5f)
+        if (Math.Abs(Input.GetAxisRaw(horizontal)) > 0.5f)
         {
             this.transform.Translate(new Vector3(Input.GetAxisRaw(horizontal) * speed * Time.deltaTime,0,0));
+            isWalking = true;
+            lastMovement = new Vector2(Input.GetAxisRaw(horizontal), 0);
         }
 
         if (Math.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
         {
             this.transform.Translate(new Vector3(0, Input.GetAxisRaw(vertical) * speed * Time.deltaTime, 0));
+            isWalking = true;
+            lastMovement = new Vector2(0, Input.GetAxisRaw(vertical));
         }
+
+
+        animator.SetBool(animp_isWalking, isWalking);
+        animator.SetFloat(animp_LastHorizontal, lastMovement.x);
+        animator.SetFloat(animp_LastVertical, lastMovement.y);
+        animator.SetFloat(horizontal, Input.GetAxisRaw(horizontal));
+        animator.SetFloat(vertical, Input.GetAxisRaw(vertical));
     }
 }
