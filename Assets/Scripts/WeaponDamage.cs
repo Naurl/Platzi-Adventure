6be12dfa2491652dabefb1dda9ed52dda5c8c563
Nahuel Venencio <nahuelvenencio@gmail.com>
2020-06-06 19:56:55 -0300

    Agregado de prefab que utilizaremos como canvas y agregado de efecto al golpear a los enemigos.

--- Assets/Scripts/WeaponDamage.cs
+++ Assets/Scripts/WeaponDamage.cs
@@ -6,12 +6,17 @@ public class WeaponDamage : MonoBehaviour
 {
     public int damage;
 
+    public GameObject hurtAnimation;
+    public GameObject HitPoint;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Enemy"))
         {
             //Destroy(collision.gameObject);
             collision.gameObject.GetComponent<HealthManager>().DamageCharacter(damage);
+
+            Instantiate(hurtAnimation, HitPoint.transform.position, HitPoint.transform.rotation);
         }
     }
 }
